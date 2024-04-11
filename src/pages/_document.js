f3710e5c9d28d1c4f837ca0e7f63ca6e913e656e
pranjal-betaflux <pranjal.shukla@betaflux.co>
2024-04-11 22:32:08 +0200

    completed landing page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,29 +3,36 @@ import Image from "next/image";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body> 
-        <nav className=" bg-black-800 p-4 flex justify-between items-center">
-          <div className=" flex gap-4 items-center font-Russo text-yellow-500 text-xl font-bold">
-            <Image src="/images/logo.png" width={150} height={150} alt="logo" />
-            <div className="flex gap-8">
-            <div>
-              Home
+      <body>
+        <nav className='border-2 border-gray-200 p-2 flex justify-between items-center'>
+          <div className=' flex gap-4 items-center font-Russo text-yellow-500 text-xl font-bold'>
+            <Image src='/images/logo.png' width={150} height={150} alt='logo' />
+            <div className='flex gap-36'>
+              <div>Home</div>
+              <div>About us</div>
             </div>
-            <div>
-              About us
-            </div>
-            </div>
-            
           </div>
-          <div className=" flex gap-8 items-center">
-            <button className="bg-yellow-500 rounded-full px-3 py-2 text-md font-bold">
-              Become a Talent
-            </button>
-            <button className="bg-yellow-500 rounded-full px-3 py-2 text-md font-bold">
-              Become a Recruiter
-            </button>
+          <div className=' flex gap-8 items-center'>
+            <div className='flex gap-2 items-center'>
+              <Image
+                src='/assets/icons/mail.svg'
+                width={20}
+                height={20}
+                alt='call'
+              />
+              <div className='font-bold'>Queries</div>
+            </div>
+            <div className='flex gap-2 items-center'>
+              <Image
+                src='/assets/icons/call.svg'
+                width={20}
+                height={20}
+                alt='call'
+              />
+              <div className='font-bold'>662464803 </div>
+            </div>
           </div>
         </nav>
         <Main />

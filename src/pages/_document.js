13fba774c7042441fdf6b2e9246ea20cc895f992
Pranjal Shukla <pranjal.shukla@betaflux.co>
2024-04-19 09:29:18 +0200

    changes in navbar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,13 +5,17 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <body>
-        <nav className='border-2 border-gray-200 p-2 flex justify-between items-center'>
-          <div className=' flex gap-4 items-center font-Russo text-yellow-500 text-xl font-bold'>
-            <Image src='/images/logo.png' width={150} height={150} alt='logo' />
-            <div className='flex gap-36'>
-              <div>Home</div>
-              <div>About us</div>
+      <body> 
+        <nav className=" bg-black-800 px-4 flex justify-between items-center border-b-2 border-b-gray-100">
+          <div className=" flex gap-8 items-center font-Russo text-slate-700  text-xl font-extrabold">
+            <Image src="/images/logo.png" width={150} height={150} alt="logo" />
+            <div className="flex gap-12">
+            <div className="cursor-pointer hover:text-black">
+              Home
+            </div>
+            <div className="cursor-pointer hover:text-black">
+              About us
+            </div>
             </div>
           </div>
           <div className=' flex gap-8 items-center'>
